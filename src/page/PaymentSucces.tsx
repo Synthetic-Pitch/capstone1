a24671cf162b7379869d00bcff630bbb01cea459
reymark <genuinepower123@gmail.com>
2026-04-29 19:51:38 +0800

    main

--- src/page/PaymentSucces.tsx
+++ src/page/PaymentSucces.tsx
@@ -1,15 +1,16 @@
 import { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
+
 const PaymentSucces = () => {
     const [searchParams] = useSearchParams();
     
     const tid = searchParams.get('tid');  // transaction.UUID
     const iid = searchParams.get('iid');  // intentId
-    const paymentIntentId = searchParams.get('payment_intent_id');
+
     useEffect(()=>{
-        console.log(tid,iid,paymentIntentId);
+        console.log("tid:",tid,"iid:",iid);
         
-    },[tid,iid,paymentIntentId])
+    },[tid,iid])
     return (
         <div>PaymentSucces</div>
     );
