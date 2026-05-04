747bd356dbb5399522fd72eba349ff8d1213223e
reymark <genuinepower123@gmail.com>
2026-05-04 20:15:59 +0800

    main

--- src/page/PaymentSucces.tsx
+++ src/page/PaymentSucces.tsx
@@ -9,19 +9,21 @@ const PaymentSucces = () => {
     const tid = searchParams.get('tid');
     const iid = searchParams.get('iid'); // PayMongo appends this automatically
     const plate_number = searchParams.get('pln');
+    const payment_method = searchParams.get('pm');
 
     const { mutate: verifyPayment, isPending, isSuccess, isError } = useVerifyPayment();
     
     useEffect(() => {
-        if (!tid || !iid || !plate_number) return;
+        if (!tid || !iid || !plate_number || !payment_method) return;
         verifyPayment({
             intent_id: iid,
             transaction_id: tid,
             plate_number: plate_number,
+            payment_method: payment_method 
         });
-    }, [tid, iid, plate_number]);
+    }, [tid, iid, plate_number, payment_method]);
 
-    if (!tid || !iid || !plate_number) return <PageNotFound />;
+    if (!tid || !iid || !plate_number || !payment_method) return <PageNotFound />;
 
     if (isPending) return <div>Verifying your payment...</div>;
     if (isError)   return <div>Something went wrong. Please contact support.</div>;
