b020424e4121f7ece70ab7a131fe1be4e0857522
reymark <genuinepower123@gmail.com>
2026-05-20 17:18:13 +0800

    main

--- src/page/ProcessViolation.tsx
+++ src/page/ProcessViolation.tsx
@@ -156,7 +156,7 @@ const ProcessViolation = () => {
                     {file1 && file2 && (
                         <button
                             onClick={onSubmit}
-                            className="text-2xl bg-[#0b318f] max-w-min m-auto px-42 py-4 text-white rounded-2xl mt-12 cursor-pointer hover:scale-[1.05] transition-all duration-100"
+                            className="text-2xl bg-[#0b318f] max-w-min m-auto px-32 tablet:px-42 py-4 text-white rounded-2xl mt-12 cursor-pointer hover:scale-[1.05] transition-all duration-100"
                         >
                             submit
                         </button>
